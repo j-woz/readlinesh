bef901644cb6b550a068f1b95c4f167f6b69d8e3
Justin Wozniak <woz@anl.gov>
2023-03-30 09:17:06 -0500

    Add string.h

--- src/readlinesh.c
+++ src/readlinesh.c
@@ -2,8 +2,9 @@
 #define _GNU_SOURCE // for asprintf()
 #include <getopt.h>
 #include <signal.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
